44f4b7e19178b46aba684834110a1c5eb1187eeb
AZMSHA <azmsha2003@gmail.com>
2024-01-20 15:23:22 +0500

    added logo to navbar

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,10 +1,12 @@
 import "./navbar.scss"
 import Button from "../Button/button"
+import Logo from "../../assets/Logo.png"
 
 function Navbar({links}) {
     return(
     <nav>
         <ul>
+            <img src={Logo} alt="" />
             {links.map((link,index)=>{
                 if(index===links.length-1){
                     return <Button key={index} Text={link} Class={"btn-pri"}/>
