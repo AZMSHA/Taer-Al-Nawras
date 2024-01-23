b08a97aa7a22d16040c5965442f35c742c360fa4
AZMSHA <azmsha2003@gmail.com>
2024-01-23 11:06:37 +0500

    -- replaced logo.png with variating svgs -- added shine animation to button -- Installed SVGR to convert SVGs to react components

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,12 +1,12 @@
 import "./navbar.scss"
 import Button from "../Button/button"
-import Logo from "../../assets/Logo.png"
+import Logo from "../../assets/LogoNoOutline.svg"
 
 function Navbar({links}) {
     return(
     <nav>
         <ul>
-            <img src={Logo} alt="" />
+            <img src={Logo} alt="Taer Al Nawras" />
             {links.map((link,index)=>{
                 if(index===links.length-1){
                     return <Button key={index} Text={link} Class={"btn-pri"}/>
