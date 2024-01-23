35b38d1cc662071a445df9724f80b6d79c069702
AZMSHA <azmsha2003@gmail.com>
2024-01-23 22:12:08 +0500

    -- Added two spinning spheres to homepage -- changed background image of first section of homepage

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,7 +1,24 @@
 import "./home.scss"
-
+import CircleBig from "../../BoilerImages/circle-big.png"
+import CircleSmall from "../../BoilerImages/circle-small.png"
 function Home() {
-    return <div>This is the home page</div>
+    return (
+        <main>
+            <section className="main-sec1">
+                <img className="circleB" src={CircleBig} alt="" />
+                <div className="circleWrapper"><img className="circleS" src={CircleSmall} alt="" /></div>
+            </section>
+            <section className="main-sec2">
+                second Section
+            </section>
+            <section className="main-sec3">
+                third Section
+            </section>
+            <section className="main-sec4">
+                Fourth Section
+            </section>
+        </main>
+    )
 }
 
 export default Home
