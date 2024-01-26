4adc69d6edcccbbc6485001b44eca1a37183cd57
AZMSHA <azmsha2003@gmail.com>
2024-01-27 00:34:19 +0500

    -- Added animated header text to landing section

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -2,11 +2,33 @@ import "./home.scss"
 import CircleBig from "../../BoilerImages/circle-big.png"
 import CircleSmall from "../../BoilerImages/circle-small.png"
 function Home() {
+
+    function letterfy(str) {
+        const charArray = Array.from(str);
+        const letters = charArray.map((char, index) => {
+        return <span className={"letter"+index}>{char}</span>
+        })
+        return letters
+    };
+
     return (
         <main>
             <section className="main-sec1">
-                <img className="circleB" src={CircleBig} alt="" />
-                <div className="circleWrapper"><img className="circleS" src={CircleSmall} alt="" /></div>
+                <div className="balls">
+                    <img className="circleB" src={CircleBig} alt="" />
+                    <span className="center"><img className="circleS" src={CircleSmall} alt="" /></span>
+                </div>
+                <header>
+                    <h6>
+                        <div className="sentence1">
+                            {letterfy("Creativity&")}
+                        </div>
+                        <div className="sentence2">
+                            {letterfy("Functionality")}
+                        </div>
+                    </h6>
+                    <p>Such a massive increase of year-over-year revenue can only mean one thing for a digital marketing company.</p>
+                </header>
             </section>
             <section className="main-sec2">
                 second Section
